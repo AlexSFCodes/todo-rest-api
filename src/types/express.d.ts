0665877d5923611fadd5461a3076523f4b55fe76
Alexis Dariel <darixsango@hotmail.com>
2026-03-18 16:41:10 -0500

    cambios necesarios en para que la app corra se modifico el ts config que afectaba al ts node

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,5 +1,12 @@
-declare namespace Express {
-    interface Request {
-        user?: { id: string }
+// src/types/express.d.ts
+import { JwtPayload } from "jsonwebtoken"
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: { id: string }
+        }
     }
 }
+
+export {}
